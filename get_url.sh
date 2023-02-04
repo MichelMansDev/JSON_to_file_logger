c9d20fe339df99aab5438d82ba17179f360da311
MichelMansDev <74714378+MichelMansDev@users.noreply.github.com>
2023-02-05 00:53:38 +0100

    Update get_url.sh
    Made it better: more comments, more variables.

--- get_url.sh
+++ get_url.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
+# This program relies on the JSON parsing with 'jq'. To install, use 'apt install jq' and off you go
+
+# define the URL to grab. This should be a... JSON format...
+
 SCORE_URL="https://filesamples.com/samples/code/json/sample2.json"
+
+# define the 2 seperate files to write to
 
 OUTFILE_HOME="./score_home.txt"
 OUTFILE_GUEST="./score_guest.txt"
+
+# change the JSON-tag to address the correct one 
+
+JSON_TAG_HOME=".age"
+JSON_TAG_GUEST=".age"
+
+# how long do we sleep before we get new data (seconds)
+
+SLEEP_SECS=5
+
+# Do not make changes from here on...
 
 while true
 do
@@ -13,8 +30,8 @@ do
 
     echo -n $TIMESTAMP
     echo -n " - "
-    #SCORE_HOME=`curl --silent $SCORE_URL | jq --raw-output .age`
-    SCORE_HOME=`echo "$OUTPUT" | jq --raw-output .age`
+    
+    SCORE_HOME=`echo "$OUTPUT" | jq --raw-output $JSON_TAG_HOME`
 
     case $SCORE_HOME in
         ''|*[!0-9]*) echo "Invalid score for 'Home', disregarding, got: '$SCORE_HOME'" ;;
@@ -22,8 +39,7 @@ do
            echo $SCORE_HOME > $OUTFILE_HOME ;;
     esac
 
-    #SCORE_GUEST=`curl --silent $SCORE_URL | jq --raw-output .firstname`
-    SCORE_GUEST=`echo "$OUTPUT" | jq --raw-output .age`
+    SCORE_GUEST=`echo "$OUTPUT" | jq --raw-output $JSON_TAG_GUEST`
 
     case $SCORE_GUEST in
         ''|*[!0-9]*) echo "Invalid score for 'Guest', disregarding, got: '$SCORE_GUEST'" ;;
@@ -31,6 +47,6 @@ do
            echo $SCORE_GUEST > $OUTFILE_GUEST ;;
     esac
 
-    sleep 5
+    sleep $SLEEP_SECS
 
 done
